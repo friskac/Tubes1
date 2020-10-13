d5d3e6d0fde74c7a4c080e8c792da9ce6149e5cf
denisestevani <56673734+denisestevani@users.noreply.github.com>
2020-10-14 00:18:59 +0700

    [Add]
    tambah buat food, sama xml food dan menu

---
+++ app/src/main/java/com/example/makanapa/FoodListAdapter.java
@@ -0,0 +1,4 @@
+package com.example.makanapa;
+
+public class FoodListAdapter {
+}
---
+++ app/src/main/java/com/example/makanapa/MockFood.java
@@ -0,0 +1,10 @@
+package com.example.makanapa;
+
+public class MockFood {
+    public static String[] foodStringArr = { "Nasi Goreng Biasa", "Nasi Goreng Telor", "Nasi Goreng Ayam",
+             };
+    public static Food[] foodObjectArr = {
+            new Food("Ayam Goreng","detail makanan "),
+            new Food("Nasi Goreng","detail makanan ")
+    };
+}
